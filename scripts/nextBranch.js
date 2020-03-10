77beec3c88e9493c27ea49b812d3a938f00c225a
Jacob Parish <jacob.parish.1@gmail.com>
2020-03-09 22:09:51 -0500

    Finishing basic merge script

--- scripts/nextBranch.js
+++ scripts/nextBranch.js
@@ -1,16 +1,19 @@
 const { execSync } = require('child_process');
 const semver = require('semver');
 
-const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString();
+const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 const currentVersion = currentBranch.slice(currentBranch.indexOf('/') + 1).trim();
 
 const allBranches = execSync('git branch -r --format=%(refname:short)').toString();
 
-const releaseBranches = allBranches
+const [nextBranch] = allBranches
   .split(/\r?\n/)
   .map(branch => branch.slice(branch.indexOf('/') + 1).trim())
   .filter(branch => branch && branch.startsWith('release'))
-  .map(branch => branch.slice(branch.indexOf('/') + 1))
-  .filter(version => version > currentVersion);
+  .filter(branch => branch > currentBranch);
 
-console.log(currentVersion, releaseBranches)
+console.log(currentBranch, nextBranch);
+
+execSync(`git checkout ${nextBranch}`);
+execSync(`git merge ${currentBranch}`);
+execSync(`git push origin ${nextBranch}`);
