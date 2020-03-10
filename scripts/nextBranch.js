24c2c5e592727e79ec68d0f173a9ce272fe363d2
Jacob Parish <jacob.parish.1@gmail.com>
2020-03-09 22:17:19 -0500

    Handle last branch in chain

--- scripts/nextBranch.js
+++ scripts/nextBranch.js
@@ -3,13 +3,20 @@ const { execSync } = require('child_process');
 const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 const currentVersion = currentBranch.slice(currentBranch.indexOf('/') + 1).trim();
 
-const allBranches = execSync('git branch -r --format=%(refname:short)').toString();
+if (currentBranch === 'develop' || !currentBranch.startsWith('release')) {
+  return;
+}
 
-const [nextBranch] = allBranches
+const allBranches = execSync('git branch -r --format=%(refname:short)').toString();
+let [nextBranch] = allBranches
   .split(/\r?\n/)
   .map(branch => branch.slice(branch.indexOf('/') + 1).trim())
   .filter(branch => branch && branch.startsWith('release'))
   .filter(branch => branch > currentBranch);
+
+if (!nextBranch) {
+  nextBranch = 'develop';
+}
 
 execSync(`git checkout ${nextBranch}`);
 execSync(`git merge ${currentBranch}`);
