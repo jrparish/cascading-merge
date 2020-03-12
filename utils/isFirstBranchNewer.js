6ee0246d05d29f2c5b767edf11653520306b25b6
Jason Stratman <jason.stratman@ncr.com>
2020-03-12 12:02:15 -0500

    Implement version comparison algorithm (#1)
    * feat: implement version comparison algorithm
    
    * newline at end of file
    
    * fix comment

---
+++ utils/isFirstBranchNewer.js
@@ -0,0 +1,86 @@
+/**
+ * This algorithm is based on Bitbucket's Branch ordering algorithm
+ * https://confluence.atlassian.com/bitbucketserver/automatic-branch-merging-776639993.html#Automaticbranchmerging-ordering
+ *
+ * Only branches that begin with the prefix 'release/' are examined
+ * 
+ * Branch names are split into tokens using any of these characters: underscore '_', hyphen '-',
+ *  plus '+', or period '.'
+ * 
+ * Branches are ordered by number, if a given token is numeric. When comparing a numeric token
+ *  with an ASCII token, the numeric is ranked higher (i.e. is considered as being a newer
+ *  version)
+ * 
+ * If both tokens are non-numeric, a simple ASCII comparison is used
+ */
+export default function isFirstBranchNewer(branchA, branchB) {
+  for (const [index, branchAToken] of branchA.tokens.entries()) {
+    const branchBToken = branchB.tokens[index];
+
+    /**
+     * Longer version always come after shorter versions if they are
+     * equivalent up to that point.
+     *
+     * Example: 5.6 comes before 5.6.1
+     */
+    if (!branchBToken) {
+      return true;
+    }
+
+    if (isNaN(branchAToken)) {
+      if (isNaN(branchBToken)) {
+        /**
+         * If both tokens are non-numeric perform string comparison
+         */
+        if (branchBToken < branchAToken) {
+          // 5.6-alpha comes before 5.6-beta
+          return true;
+        } else if (branchAToken === branchBToken) {
+          // Equal, keep checking
+          continue;
+        }
+
+        return false;
+      } else {
+        /**
+         * Numeric tokens are newer than non-numeric tokens
+         *
+         * Example: 6.1-SNAPSHOT is older than 6.1.0
+         */
+        return false;
+      }
+    } else {
+      /**
+       * Numeric tokens are newer than non-numeric tokens
+       *
+       * Example: 6.1-SNAPSHOT comes before 6.1.0
+       */
+      if (isNaN(branchBToken)) {
+        return true;
+      } else {
+        const branchAInt = parseInt(branchAToken);
+        const branchBInt = parseInt(branchBToken);
+
+        /**
+         * Perform numeric comparison
+         */
+        if (branchBInt < branchAInt) {
+          // Example: 5 comes before 6
+          return true;
+        } else if (branchAInt === branchBInt) {
+          // Equal, keep checking
+          continue;
+        }
+
+        return false;
+      }
+    }
+  }
+
+  /**
+   * Reaching this point means the branchB version had all of the branchA
+   * version's tokens and then some, meaning the branchB version is
+   * newer (later) than the branchA version.
+   */
+  return false;
+}
